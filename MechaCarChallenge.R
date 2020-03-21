6b2a37b3e633d9c1863dfff1ef8d190c458e7612
Priyanka Senapati <write2priyankasenapati@gmail.com>
2020-03-21 20:43:48 +0530

    Addong MechaCarChallenge.R

---
+++ MechaCarChallenge.R
@@ -0,0 +1,32 @@
+## (1) MPG REGRESSION 
+#Read MechaCar data
+mecha_car <- read.csv('MechaCar_mpg.csv')
+#Perform mulitple linear regression with continuous data
+lm(mpg ~ vehicle.length + vehicle.weight + spoiler.angle + ground.clearance + AWD,data=mecha_car)
+#Get r-squared and p-value
+summary(lm(mpg ~ vehicle.length + vehicle.weight + spoiler.angle + ground.clearance + AWD,data=mecha_car))
+#Cross check to see if the results are repeated with only the factors that provide a non-random amout of variance.
+lm(mpg ~ vehicle.weight + spoiler.angle, data=mecha_car)
+summary(lm(mpg ~ vehicle.weight + spoiler.angle, data=mecha_car))
+
+
+## (2) SUSPENSION COIL SUMMARY
+# Read file
+suspension_coil <- read.csv(file='Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
+# Create summary statistics table for suspension coil's PSI
+summary_table <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarise(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Variane=var(PSI), Standard_Deviation=sd(PSI))
+
+
+## (3)SUSPENSION COIL T-Test
+# Create subsets
+#Filter suspension_coil data by Lot
+sample_lot1 <- suspension_coil %>%
+  filter(Manufacturing_Lot=='Lot1')
+sample_lot2 <- suspension_coil %>%
+  filter(Manufacturing_Lot=='Lot2')
+sample_lot3 <- suspension_coil %>%
+  filter(Manufacturing_Lot=='Lot3')
+#Calculate Independent (one Sample) T-Test
+t.test(sample_lot1$PSI, mu=1500)
+t.test(sample_lot2$PSI, mu=1500)
+t.test(sample_lot3$PSI, mu=1500)
